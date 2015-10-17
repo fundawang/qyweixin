5a9941f0d9210655af5f1e071536485c72a93988
Funda Wang <fundawang@gmail.com>
2015-10-17 14:58:20 +0800

    provides material methods

--- src/Entity/AgentBase.php
+++ src/Entity/AgentBase.php
@@ -28,6 +28,12 @@ use Drupal\qyweixin\AgentInterface;
  * )
  */
 Class AgentBase extends ConfigEntityBase implements AgentInterface {
+	const MATERIAL_TYPE_MPNEWS='mpnews';
+	const MATERIAL_TYPE_IMAGE='image';
+	const MATERIAL_TYPE_VOICE='voice';
+	const MATERIAL_TYPE_VIDEO='video';
+	const MATERIAL_TYPE_FILE='file';
+	
 	protected $agentid=NULL;
 	protected $id=NULL;
 	protected $token=NULL;
@@ -145,4 +151,36 @@ Class AgentBase extends ConfigEntityBase implements AgentInterface {
 			return $this;
 		}
 	}
+
+	/**
+	 * Get list of materials of this agent
+	 *
+	 * @param const type
+	 *
+	 * @param int offset
+	 *
+	 * @param int count
+	 *
+	 * @return stdClass
+	 */
+	public function materialBatchGet($type=MATERIAL_TYPE_IMAGE, $offset=0, $count=10) {
+		try {
+			$ret=new stdClass();
+			$access_token=\Drupal\qyweixin\Corp::getAccessToken();
+			$body->agentid=$this->getAgentId();
+			$url=sprintf('https://qyapi.weixin.qq.com/cgi-bin/material/batchget?access_token=%s', $access_token);
+			$data = (string) \Drupal::httpClient()->post($url, ['body'=>json_encode($body, JSON_UNESCAPED_UNICODE)])->getBody();
+			$response=json_decode($data);
+			if(empty($response)) throw new \Exception(json_last_error_msg(), json_last_error());
+			if($response->errcode) throw new \Exception($response->errmsg, $response->errcode);
+			$ret=$response;
+			unset($ret->errcode);
+			unset($ret->errmsg);
+		} catch (\Exception $e) {
+			throw new \Exception($e->getMessage(), $e->getCode());
+		} finally {
+			return $ret;
+		}
+	}
+
 }
