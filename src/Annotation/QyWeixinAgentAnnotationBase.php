c1bd5c110cd758c8705784d7515e31b6695c3507
Funda Wang <fundawang@gmail.com>
2015-10-20 10:49:38 +0800

    add annotationbase plugin

---
+++ src/Annotation/QyWeixinAgentAnnotationBase.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\qyweixin\Annotation\QyWeixinAgentAnnotationBase.
+ */
+
+namespace Drupal\qyweixin\Annotation;
+
+use Drupal\Component\Annotation\Plugin;
+
+/**
+ * Defines an qyweixin agent object.
+ *
+ * Plugin Namespace: Plugin\QyWeixinAgent
+ *
+ * @Annotation
+ */
+class QyWeixinAgentAnnotationBase extends Plugin {
+
+  /**
+   * The plugin ID.
+   *
+   * @var string
+   */
+  public $id;
+
+}
