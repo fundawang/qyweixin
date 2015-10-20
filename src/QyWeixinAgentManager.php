fa00c85ce9a5a6968b7178d2365f2012f8212b73
Funda Wang <fundawang@gmail.com>
2015-10-20 10:49:18 +0800

    rename to annotationbase

--- src/QyWeixinAgentManager.php
+++ src/QyWeixinAgentManager.php
@@ -29,14 +29,10 @@ class QyWeixinAgentManager extends DefaultPluginManager {
 	*   The module handler.
 	*/
 	public function __construct(\Traversable $namespaces, CacheBackendInterface $cache_backend, ModuleHandlerInterface $module_handler) {
-		parent::__construct('Plugin/QyWeixinAgent', $namespaces, $module_handler, 'Drupal\qyweixin\AgentInterface', 'Drupal\qyweixin\Annotation\QyWeixinAgent');
+		parent::__construct('Plugin/QyWeixinAgent', $namespaces, $module_handler, 'Drupal\qyweixin\AgentInterface', 'Drupal\qyweixin\Annotation\QyWeixinAgentAnnotationBase');
 
 		$this->alterInfo('qyweixin_agent_info');
 		$this->setCacheBackend($cache_backend, 'qyweixin_plugins');
 	}
 	
-	public function getAccessToken() {
-		return \Drupal\qyweixin\CorpBase::getAccessToken();
-	}
-
 }
